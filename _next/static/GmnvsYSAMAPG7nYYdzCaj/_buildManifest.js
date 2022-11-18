848172a0bf0fb983cf044bcb6cb777bc00a3b2eb
gavfu <gavfu@users.noreply.github.com>
2022-11-18 09:22:04 +0000

    deploy: 620e1a4f4c8e1db2c2e5d77224ac667a11868194

---
+++ _next/static/GmnvsYSAMAPG7nYYdzCaj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/00c939ce301ccd8a.css","static/chunks/pages/index-b50cb1eb9f79349b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
